feb426373fb3a4795446032ec4ef6192ff1b4144
Unword112 <pog123@hotmail.co.uk>
2022-04-22 14:32:02 +0700

    'fourth'

--- src/components/CategoryCard.js
+++ src/components/CategoryCard.js
@@ -6,7 +6,14 @@ export default function CategoryCard({ category: {name, image } }) {
     return(
         <div>
             <p>{name}</p>
-            
+            <Link to={`/category/${name}`} className='text-base text-blue-500'>
+                <img 
+                    src={image}
+                    alt={name}
+                    classNmae='w-48 h-48 my-4 lg:my-6 mx-auto'
+                />
+                <p>Shop now</p>
+            </Link>
         </div>
     )
 }
---
+++ src/components/ProductSlider.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import { Link } from 'react-router-dom';
+
+export default function ProductSlider({ title, products }){
+    retrun(
+        <div className='bg-white p-4'>
+            <span className='font-bold text-md text-gray-600 mr-4'>{title}</span>
+            <a href='#' className='text-base text-blue-500'>Shop now</a>
+            <div 
+                style={{
+                    gridTemplateColumns: `repeat(${products.length}, calc(20% - 1rem * ${ 2 / window.innerWidth}))`,
+                }}
+                className={`grid gap-2 md:gap-4 grid-rows-1 overflow-x-scroll whitespace-nowrap my-4`}>
+                {React.Children.toArray(
+                    products.map((product) => (
+                        <Link>
+                            <img 
+                                src={product.image}
+                                alt={product.name}
+                                className='h-24 md:w-24 md:my-3'
+                            />
+                        </Link>
+                    ))
+                )}
+            </div>
+        </div>
+    )
+}
